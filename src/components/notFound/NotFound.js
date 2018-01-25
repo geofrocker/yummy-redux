79c9842e11f145c25fd95d8ae2a731870b1454c1
geofrocker <geofrocker2@gmail.com>
2018-01-25 10:08:08 +0300

    [ft #154623349] Add a not found folder for 404 requests

---
+++ src/components/notFound/NotFound.js
@@ -0,0 +1,11 @@
+// components/NotFound.js
+import React from 'react';
+
+const NotFound = () =>
+  <div class="NotFound">
+    <h3>404 page not found</h3>
+    <p>We are sorry but the page you are looking for does not exist.</p>
+    <a className="btn btn-primary" href="/">Take me Home</a>
+  </div>
+
+export default NotFound;
