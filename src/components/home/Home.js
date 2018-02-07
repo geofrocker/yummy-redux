5b385a2da5a429df4f32f7eb62c4abe4d4771ae6
geofrocker <geofrocker2@gmail.com>
2018-02-07 22:32:11 +0300

    Update/Add more actions for the home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,94 +1,62 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
-import * as homeRecipesActions from '../../actions/homeRecipesActions'
+import {Redirect} from 'react-router-dom'
+import toastr from 'toastr'
+import { loadRecipes} from '../../actions/recipesActions'
 import Recipes from './Recipes'
 import url from '../../config'
 class Home extends Component {
-    constructor(props, context){
+    constructor(props){
         super(props);
-        this.state = {
-            data:[],
-            showMessage:false, 
-            q:'', 
-            page:1, 
-            has_next:false, 
-            next_page:'',
-            previous_page:'', 
-            disablePrevious:'', 
-            disableNext:'',
-            url: url+'?page=1',
-            pages:null
-        };
-        this.props.actions.loadPublicRecipes(this.state.url)
-    }
-    
-    //Load recipes before the component recieve props
-    componentWillReceiveProps(nextProps){
-        this.setState({
-            data:nextProps.data.Recipe_list,
-            showMessage:false,
-            pages: nextProps.data.total_pages
-        })
-        if(nextProps.data.previous_page === 'Null'){
-            this.setState({
-                disablePrevious: 'page-item disabled',
-                previous_page: ''
-                });
-        }else{
-            this.setState({
-                previous_page: nextProps.data.previous_page,
-                disablePrevious: 'page-item'
-                });
-        }
-        if(nextProps.data.next_page === 'Null'){
-            this.setState({
-                disableNext: 'page-item disabled',
-                next_page: '',
-                });
-        }else{
-            this.setState({
-                next_page: nextProps.data.next_page,
-                disableNext: 'page-item'
-                });
-        }
+        this.state = {showMessage:false, q:'', page:1, url: url+'?page=1', redirect:false};
+        this.props.loadRecipes(this.state.url)
+        .catch(xhr =>{
+            let defaultResponse = "An Error occurred, Try again";
+            toastr.error(xhr.response ? xhr.response.data.message: defaultResponse)
+            this.setState({redirect:true})
+            console.log(this.state.redirect)
+          })
     }
 
     //Handle the link to the next page from pagination
     nextPage() {
-        if (this.state.next_page)
-            this.props.actions.loadPublicRecipes(this.state.next_page)
-            return this.setState({
-                page:this.state.next_page[this.state.next_page.length -1]
-            })
+        if (this.props.has_next){
+            this.props.loadRecipes(this.props.next_page)
+        }
     }
 
     // Handle the link to the previous page from pagination
     previousPage() {
-        if(this.state.previous_page)
-        this.props.actions.loadPublicRecipes(this.state.previous_page)
-        return this.setState({
-            page:this.state.previous_page[this.state.previous_page.length -1]
-        })
+        if(this.props.has_prev){
+            this.props.loadRecipes(this.props.previous_page)
+        }
     };
 
     // Handle the search functionality
     handleSearch = (event) => {
         event.preventDefault();
-        this.setState({
-            q: event.target.value,
-            page:1
-        });
+        this.setState({showMessage:false,q: event.target.value,page:1});
         if(this.state.q){
             let localurl = url +'?q='+this.state.q+'&page='+this.state.page
-            this.props.actions.loadPublicRecipes(localurl)
+            this.props.loadRecipes(localurl)
+        }else{
+            let localurl = url +'?page='+this.state.page
+            this.props.loadRecipes(localurl)
+        }
+        if(this.props.recipes.length===0){
+            this.setState({
+                showMessage:true
+            })
         }
     }
     render(){
-        const {data, disableNext, disablePrevious, page, pages, showMessage, }=this.state
+        const {page, recipes, disableNext, disablePrevious, total_pages}=this.props
+        if(this.state.redirect){
+            return <Redirect to='/error'/>
+        }
         let loadPagination;
-        if (data.length>0) {
+        if (recipes.length>0) {
             loadPagination =
            <div className="col-xs-11 col-sm-3 pull-right">
                 <ul className="pagination">
@@ -97,15 +65,15 @@ class Home extends Component {
                     </li>
                     <li className={disableNext}>
                     <a className="page-link" onClick={() => this.nextPage()}>Next</a>
-                    <a className="page-link">Showing {page} of {pages}</a>
+                    <a className="page-link">Showing {page} of {total_pages}</a>
                     </li>
                     
                 </ul>
             </div> 
         }
-        let loadNavBarContent;
-        if (showMessage) {
-           loadNavBarContent =
+        let loadSearchBarContent;
+        if (this.state.showMessage) {
+            loadSearchBarContent =
            <div className="Home jumbotron" id="search-bar">
                 <h3>No recipes matched your query.</h3>
             </div>
@@ -119,9 +87,9 @@ class Home extends Component {
                     </div>
                 </div>
                 <div className="col-xs-12">
-                {loadNavBarContent}
+                {loadSearchBarContent}
                 <hr/>
-                    {data.map(inf =>
+                    {recipes.map(inf =>
                     <Recipes key={inf.recipe_id} {...inf}/>
                     )}
                 </div>
@@ -132,18 +100,39 @@ class Home extends Component {
 }
 
 Home.propTypes = {
-    data: PropTypes.object.isRequired,
+    recipes: PropTypes.array.isRequired,
   }
   
   function mapStateToProps(state, ownProps){
+      let disableNext = ""
+      let disablePrevious = ""
+      if(state.recipes.has_next){
+        disableNext = "page-item"
+      }else{
+        disableNext = "page-item disabled" 
+      }
+      if(state.recipes.has_prev){
+        disablePrevious = "page-item"
+      }else{
+        disablePrevious = "page-item disabled"
+      }
+
     return {
-      data:state.publicRecipes
+        recipes:state.recipes.recipes,
+        page:state.recipes.page,
+        total_pages:state.recipes.total_pages,
+        has_next:state.recipes.has_next,
+        has_prev:state.recipes.has_prev,
+        previous_page:state.recipes.previous_page,
+        next_page:state.recipes.next_page,
+        message:state.message,
+        disablePrevious,
+        disableNext
     }
   }
   
-  function mapDispatchToProps(dispatch){
-    return{
-      actions: bindActionCreators(homeRecipesActions, dispatch)
-    }
-  }
+  export const mapDispatchToProps = (dispatch) => ({
+    loadRecipes:(url) => dispatch(loadRecipes(url))    
+  })
+
   export default connect(mapStateToProps, mapDispatchToProps) (Home);
