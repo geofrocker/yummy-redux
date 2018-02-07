777f6c53e7f6023ec7e2028e87446a3f1f7d986d
geofrocker <geofrocker2@gmail.com>
2018-02-07 22:27:22 +0300

    Refactor/ Update action for login

--- src/actions/loginActions.js
+++ src/actions/loginActions.js
@@ -1,19 +1,26 @@
-import * as types from './actionTypes'
-import  url, { http } from '../config'
+import * as types from './actionTypes';
+import url, { http } from '../config';
+import { beginAjaxCall } from './ajaxStatusActions';
 
-export function loginFails(message){
-  return{ type: types.LOGIN_FAILS, loginMessage:message}
+export function loginFails(message) {
+  return { type: types.LOGIN_FAIL, message };
+}
+export function loginSuccess(message) {
+  return { type: types.LOGIN_SUCCESS, message };
 }
 
-export function login(userData){
-  return function(dispatch){
-    return http.post(url+'auth/login',userData)
-        .then((response)=>{
-          localStorage.setItem('token',response.data.token)
-          localStorage.setItem('isLoggedIn',true)
-          localStorage.setItem('user', response.data.username)
-      }).catch(xhr => {
-        dispatch(loginFails(xhr.response.data.error));
+export function login(userData) {
+  return function (dispatch) {
+    dispatch(beginAjaxCall());
+    return http.post(`${url}auth/login`, userData)
+      .then((response) => {
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('isLoggedIn', true);
+        localStorage.setItem('user', response.data.username);
+        dispatch(loginSuccess(response.data.token));
+      }).catch((xhr) => {
+        dispatch(loginFails(xhr));
+        throw (xhr);
       });
-  }
+  };
 }
